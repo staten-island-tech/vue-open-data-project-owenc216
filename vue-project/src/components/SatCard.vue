c3c9193884e33b84aa881cde528f3c3933e38a87
owenc216 <owen216413@gmail.com>
2026-03-18 14:02:16 -0400

    adad

---
+++ vue-project/src/components/SatCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <h3>School Name: {{ props.sat.school_name }}</h3>
+    <p>SAT Math Average Score: {{ props.sat.sat_math_avg_score }}</p>
+    <p>SAT Critical Reading Average Score: {{ props.sat.sat_critical_reading_avg_score }}</p>
+    <p>SAT Writing Average Score: {{ props.sat.sat_writing_avg_score }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  sat: Object,
+})
+</script>
+
+<style lang="scss" scoped></style>
+//? no value yet dont show anything wait until something is there
--- vue-project/src/views/SatData.vue
+++ vue-project/src/views/SatData.vue
@@ -4,17 +4,18 @@
     <input v-model="searchSchool" placeholder="Type school name" />
     <select v-model="selectedSchool">
       <option value="">select a school</option>
-      <option v-for="(school, index) in sat" :key="index" :value="school.school_name">
+      <option v-for="(school, index) in filteredOptions" :key="index" :value="school.school_name">
         {{ school.school_name }}
       </option>
     </select>
     <div class="cards">
-      <satCard v-for="(sat, index) in selectedList" :key="index" :sat="sat" />
+      <SatCard v-for="(sat, index) in selectedList" :key="index" :sat="sat" />
     </div>
   </div>
 </template>
 <script setup>
 import { onMounted, ref, watch } from 'vue'
+import SatCard from '@/components/SatCard.vue'
 const Sat = ref([])
 const searchSchool = ref('')
 const selectedSchool = ref('')
@@ -25,18 +26,20 @@ async function getSat() {
   try {
     const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
     const data = await response.json()
-    Sat.value = data.results
-    filteredOptions.value = data.results
-    selectedList.value = data.results
+    Sat.value = data
+    filteredOptions.value = data
+    selectedList.value = data
   } catch (error) {
     console.log(error)
   }
 }
-watch(searchSchool, function () {
-  filteredOptions.value = Sat.value.filter(function (school) {
-    return school.school_name.toLowerCase().includes(searchSchool.value.toLowerCase())
-  })
+watch(searchSchool, () => {
+  filteredOptions.value = Sat.value.filter((school) =>
+    school.school_name.toLowerCase().includes(searchSchool.value.toLowerCase()),
+  )
 })
+
+/*
 watch(selectedSchool, function () {
   if (selectedSchool.value === '') {
     selectedList.value = Sat.value
@@ -45,7 +48,7 @@ watch(selectedSchool, function () {
       return school.school_name === selectedSchool.value
     })
   }
-})
+}) */
 onMounted(() => {
   getSat()
 })
@@ -58,7 +61,7 @@ onMounted(() => {
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
-  align-items: center;
+  align-items: center;x
   justify-content: space-around;
 }
 </style>
